0f12dac464f56f0b7c8e7cadb7c816482002db82
DrakeStephens1999 <drake,stephens56@gmail.com>
2021-12-14 19:50:02 -0700

    finished content now i need to add Data

--- home.js
+++ home.js
@@ -1,4 +1,8 @@
+var song = document.getElementById('song');
 var slideIndex = 0;
+const quoteHtml=document.getElementById("quote");
+const author=document.getElementById("author");
+
 showSlides();
 
 function showSlides() {
@@ -11,4 +15,30 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000);
+}
+
+const getNewQuote = async () =>
+{
+    var url="https://type.fit/api/quotes";    
+
+    const response=await fetch(url);
+    const quotes = await response.json();
+
+    const index = Math.floor(Math.random()*quotes.length);
+
+    const quote=quotes[index].text;
+
+    const auth=quotes[index].author;
+
+    if(auth==null)
+    {
+        author = "Anonymous";
+    }
+
+    quoteHtml.innerHTML=quote;
+    author.innerHTML="~ "+auth;
+}
+
+function playSong() {
+  song.play();
 }
